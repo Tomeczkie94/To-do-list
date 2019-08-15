caa1f304a3180c61bd5f89cc7d877fb9862fa7f3
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2019-08-15 21:10:23 +0200

    List was working. Now empty list items are added. Require fix

--- to-do-list.js
+++ to-do-list.js
@@ -1,7 +1,16 @@
-function createList(event) {
-  const input = document.querySelector('#text-field').value;
-  const newListItem = document.createElement('li');
-  const newContent = document.createTextNode(input);
-  const checklist = document.querySelector('#checklist');
-  checklist.appendChild(newListItem);
+document.getElementById('submit-button').onclick = function() {
+  const listItem = document.createElement('li');
+  const text = document.getElementById('text-field').value;
+  const textnode = document.createTextNode(text);
+  listItem.appendChild(textnode);
+  document.getElementById('checklist').appendChild(listItem);
+  document.getElementById('my-form').reset();
 }
+
+const input = document.getElementById('text-field');
+input.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById('submit-button').click();
+  }
+});
