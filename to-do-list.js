547ce14c5f49346ec910f9210c469745d642ea67
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2019-08-20 20:03:27 +0200

    Onclick creates li with span and input

--- to-do-list.js
+++ to-do-list.js
@@ -2,10 +2,15 @@ const input = document.getElementById('text-field');
 
 const createListItem = function() {
   const listItem = document.createElement('li');
+  const listItemSpan = document.createElement('span');
+  const listItemInput = document.createElement('input');
+  listItemInput.type = 'checkbox';
   const { value: text } = input;
-  const textNode = document.createTextNode(text);
-  listItem.appendChild(textNode);
   document.getElementById('checklist').appendChild(listItem);
+  const textNode = document.createTextNode(text);
+  listItem.appendChild(listItemSpan);
+  listItem.appendChild(listItemInput);
+  listItemSpan.appendChild(textNode);
   input.value = '';
 }
 
