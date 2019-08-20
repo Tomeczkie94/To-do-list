1b43d94a870816e4462269e23405348799ea8f92
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2019-08-20 21:43:53 +0200

    Checkbox delete. Add EventListener for span checking class of the element and changing the class

--- to-do-list.js
+++ to-do-list.js
@@ -3,19 +3,26 @@ const input = document.getElementById('text-field');
 const createListItem = function() {
   const listItem = document.createElement('li');
   const listItemSpan = document.createElement('span');
-  const listItemInput = document.createElement('input');
-  listItemInput.type = 'checkbox';
+  listItemSpan.classList.add('uncompleted');
+  listItemSpan.addEventListener('click', function(event) {
+    if (listItemSpan.classList.contains('uncompleted')) {
+      listItemSpan.classList.remove('uncompleted');
+      listItemSpan.classList.add('completed');
+    }
+    else if (listItemSpan.classList.contains('completed')) {
+      listItemSpan.classList.remove('completed');
+      listItemSpan.classList.add('uncompleted');
+    }
+  });
   const { value: text } = input;
   document.getElementById('checklist').appendChild(listItem);
   const textNode = document.createTextNode(text);
   listItem.appendChild(listItemSpan);
-  listItem.appendChild(listItemInput);
   listItemSpan.appendChild(textNode);
   input.value = '';
 }
 
 document.getElementById('submit-button').onclick = createListItem;
-
 input.addEventListener('keyup', function(event) {
   if (event.keyCode === 13) {
     createListItem();
