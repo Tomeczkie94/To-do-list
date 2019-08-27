1ef92e234482f4e5b30e76b775e40dea5d9de641
Robert Harężlak <harezlak.robert7@gmail.com>
2019-08-27 19:46:49 +0200

    Extract fuction from outer function

--- to-do-list.js
+++ to-do-list.js
@@ -3,16 +3,11 @@ const input = document.getElementById('text-field');
 const createTask = function() {
   const liCreate = document.createElement('li');
   const spanCreate = document.createElement('span');
-  const classCheck = function(event) {
-    if (spanCreate.classList.contains('task-item' && 'completed')) {
-      spanCreate.classList.remove('completed');
-    }
-    else if (spanCreate.classList.contains('task-item')) {
-      spanCreate.classList.add('completed');
-    }
-  };
+
   spanCreate.classList.add('task-item');
+
   spanCreate.addEventListener('click', classCheck);
+
   const text = input.value;
   document.getElementById('checklist').appendChild(liCreate);
   const textNode = document.createTextNode(text);
@@ -20,6 +15,15 @@ const createTask = function() {
   spanCreate.appendChild(textNode);
   input.value = '';
 }
+
+const classCheck = function() {
+  if (this.classList.contains('task-item' && 'completed')) {
+    this.classList.remove('completed');
+  }
+  else if (this.classList.contains('task-item')) {
+    this.classList.add('completed');
+  }
+};
 
 document.getElementById('submit-button').onclick = createTask;
 input.addEventListener('keyup', function(event) {
